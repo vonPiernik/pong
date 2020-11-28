0a794fac51edeaaba8729ed446deda43b917e7be
Norbert Kaźmierczak <norbini12@gmail.com>
2020-11-28 14:47:14 +0100

    Move part of the code

--- src/elements/ball.js
+++ src/elements/ball.js
@@ -9,4 +9,17 @@ export class Ball extends Element {
         this.centerX += 10;
         this.centerY += 10;
     }
+
+    draw(canvas) {
+        const ctx = canvas.getContext('2d');
+        const radius = 70;
+
+        ctx.beginPath();
+        ctx.arc(this.centerX, this.centerY, radius, 0, 2 * Math.PI, false);
+        ctx.fillStyle = 'green';
+        ctx.fill();
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = '#003300';
+        ctx.stroke();
+    }
 }
--- src/modules/element.js
+++ src/modules/element.js
@@ -9,15 +9,5 @@ export class Element {
     };
 
     draw(canvas) {
-        const ctx = canvas.getContext('2d');
-        const radius = 70;
-
-        ctx.beginPath();
-        ctx.arc(this.centerX, this.centerY, radius, 0, 2 * Math.PI, false);
-        ctx.fillStyle = 'green';
-        ctx.fill();
-        ctx.lineWidth = 5;
-        ctx.strokeStyle = '#003300';
-        ctx.stroke();
     }
 }
